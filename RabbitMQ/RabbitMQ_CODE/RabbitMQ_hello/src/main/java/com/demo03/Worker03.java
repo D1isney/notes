736f23bb7eef249f1176144bcb968f0b43f7930e
Disney <2497050447@qq.com>
2024-04-11 21:31:48 +0800

    消息手动应答（消费者）
    2024年4月11日21:31:46

---
+++ RabbitMQ/RabbitMQ_CODE/RabbitMQ_hello/src/main/java/com/demo03/Worker03.java
@@ -0,0 +1,36 @@
+package com.demo03;
+
+import com.utils.RabbitMQUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import com.utils.SleepUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 消息手动应答
+ */
+public class Worker03 {
+    // 队列名称
+    public static final String TASK_QUEUE_NAME = "ack_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Channel channel = RabbitMQUtils.getChannel();
+        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        System.out.println("C1等待接收消息，处理时间较短=====");
+        DeliverCallback callback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            SleepUtils.sleep(1);
+            System.out.println("接收到消息：" + message);
+            // 1.消息标记tag
+            // 2.false代表只应答接收到的那个消息，true为应答所有消息包括传递过来的消息
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        };
+        //  手动应答
+        boolean autoAck = false;
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, callback, consumerTag -> {
+            System.out.println(consumerTag + "：消费者取消消费接口逻辑");
+        });
+    }
+}
