24d702595b92a5264eecf35fe9a1b9e3f21febc8
Disney <2497050447@qq.com>
2024-04-11 21:22:00 +0800

    线程沉睡工具类
    2024年4月11日21:21:57

---
+++ RabbitMQ/RabbitMQ_CODE/RabbitMQ_hello/src/main/java/com/utils/SleepUtils.java
@@ -0,0 +1,14 @@
+package com.utils;
+
+/**
+ * 线程沉睡工具类
+ */
+public class SleepUtils {
+    public static void sleep(int second) {
+        try {
+            Thread.sleep(1000 * second);
+        } catch (InterruptedException _ignored) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
