2ef68dc7497e1b250afd64e48ce1d157cad84ec3
Disney <2497050447@qq.com>
2024-04-11 14:03:22 +0800

    创建工具类
    2024年4月11日13:57:19

---
+++ RabbitMQ/RabbitMQ_CODE/RabbitMQ_hello/src/main/java/com/demo02/utils/RabbitMQUtils.java
@@ -0,0 +1,24 @@
+package com.demo02.utils;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 创建连接池工具类
+ */
+public class RabbitMQUtils {
+    public static Channel getChannel() throws IOException, TimeoutException {
+        // 创还能连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.129.135");
+        factory.setUsername("admin");
+        factory.setPassword("123456");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        return channel;
+    }
+}
