12f2c8cf72a2ac5ae799198dd54425c9294fe558
Disney <2497050447@qq.com>
2024-04-14 17:54:03 +0800

    整合SpringBoot
    2024年4月14日17:54:01

--- RabbitMQ/RabbitMQ_CODE/RabbitMQ_SpringBoot/src/main/java/com/rabbitmq_springboot/App.java
+++ RabbitMQ/RabbitMQ_CODE/RabbitMQ_SpringBoot/src/main/java/com/rabbitmq_springboot/App.java
@@ -1,9 +1,11 @@
 package com.rabbitmq_springboot;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class App 
 {
     public static void main( String[] args )
