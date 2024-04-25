a76ebf4384c99d26e9f3d1e9d447b0e0534458a2
Jiligu <2497050447@qq.com>
2024-04-25 21:22:10 +0800

    feat(单文件组件): 单文件组件简单用例
    没有使用脚手架并不能识别ES6语法

---
+++ Vue2/html/19_单文件组件/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+<script>
+import School from './School.vue'
+import Student from './Student.vue'
+export default {
+  name:'App',
+  components: {School},
+  data(){
+    return{}
+  },
+  methods:{},
+  Components:{
+    School,
+    Student
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ Vue2/html/19_单文件组件/School.vue
@@ -0,0 +1,37 @@
+<template>
+  <!--  组件的结构-->
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名称</button>
+  </div>
+</template>
+<script>
+//  组件交换的代码
+export default {
+// export default Vue.extend({
+// const school = Vue.extend({
+// export const school = Vue.extend({
+  name: 'School',
+  data() {
+    return {
+      name: 'Vue',
+      address: '广州'
+    }
+  },
+  methods: {
+    showName() {
+      console.log(this.name)
+    }
+  },
+}
+// })
+// export { school}
+// export default school
+</script>
+<style>
+/*组件的样式*/
+.demo {
+  background-color: orange;
+}
+</style>
