417891b348907c147aa0cc5b15f1e5d7699cd997
Jiligu <2497050447@qq.com>
2024-04-26 20:21:38 +0800

    feat(06_src_scoped样式): css局部使用
    作用：让样式在局部生效，防止冲突
    写法：<style scoped></style>

---
+++ Vue2/vue_test/06_src_scoped样式/components/School.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: 'Vue  学校123',
+      address: '广州'
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+<!--局部的-->
+<style scoped>
+.demo{
+  background-color: skyblue;
+}
+</style>
---
+++ Vue2/vue_test/06_src_scoped样式/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="demo">
+    <h2 class="test">学生名称：{{ name }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: 'Vue',
+      sex: '男'
+    }
+  },
+  methods:{},
+}
+</script>
+<style lang="less" scoped>
+.demo{
+  background-color: orangered;
+  .test{
+    font-size: 20px;
+  }
+}
+</style>
