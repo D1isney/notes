499aeb850cc8f58e4bada90144b8ba0d28721a2f
Jiligu <2497050447@qq.com>
2024-04-26 10:39:56 +0800

    feat(01_src分析脚手架): ref
    ref操作的是什么？DOM的真实对象以及vc

---
+++ Vue2/vue_test/02_src_ref属性/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 v-text="msg" id="title" ref="title"></h1>
+    <button @click="showDOM" ref="btn">点我输出上方的DOM元素</button>
+    <School ref="sch" id="sch"></School>
+  </div>
+</template>
+<script>
+import School from "./components/Schoole.vue";
+export default {
+  name:'App',
+  data(){
+    return{
+      msg:'Hello Vue'
+    }
+  },
+  methods:{
+    showDOM(){
+      //  id拿
+      console.log("真实DOM对象："+document.getElementById('title'));
+      //  ref拿
+      console.log("真实DOM对象："+this.$refs.title);
+      console.log("vc实例对象："+this.$refs)
+      console.log("vc实例对象的真实DOM对象："+document.getElementById("sch"));
+    }
+  },
+  components:{School}
+}
+</script>
+<style>
+
+</style>
