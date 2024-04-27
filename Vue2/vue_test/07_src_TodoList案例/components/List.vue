18f1cc03eb39a40a08a4c16c778b262516988664
Jiligu <2497050447@qq.com>
2024-04-27 18:52:57 +0800

    feat(07_src_TodoList案例): 案例
    组件化编程：
    （1）拆分静态组件：组件要按照功能点拆分，命名不要与HTML元素冲突
    （2）实现动态组件：考虑好数据的存放位置，数据是一个组件在使用，还是一些组件在用：
            1）一个组件在用：放在组件自身即可。
            2）一些组件在用：放在他们共同的父组件上
    （3）实现交互：从绑定事件开始
    
    props适用于：
    （1）父组件 ==》 子组件 通信
    （2）子组件 ==》 父组件（要求父组件先给子组件一个函数）
    
    使用v-model时要切记：v-model绑定的值不能是props传过来的值，因为props是不可以修改的
    
    props传过来的若是对象类型的值，修改对象中的属性时，Vue不会报错，但是不推荐这样做

---
+++ Vue2/vue_test/07_src_TodoList案例/components/List.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="todo-main">
+    <Item
+        v-for="todoObj in todos"
+        :key="todoObj.id"
+        :todo="todoObj"
+        :checkTodo="checkTodo"
+        :deleteTodo="deleteTodo"
+    />
+  </ul>
+</template>
+<script>
+import Item from "./Item.vue";
+
+export default {
+  name: 'List',
+  data() {
+    return {}
+  },
+  props: ['todos', 'checkTodo', 'deleteTodo'],
+  methods: {},
+  components: {Item}
+}
+</script>
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
