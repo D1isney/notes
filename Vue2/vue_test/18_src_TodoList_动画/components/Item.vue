fd44dd7e4c1df83ab2971746b5f5933a1c68f4ad
Jiligu <2497050447@qq.com>
2024-04-30 15:13:59 +0800

    feat(18_src_TodoList_动画): 增加删除使用动画

---
+++ Vue2/vue_test/18_src_TodoList_动画/components/Item.vue
@@ -0,0 +1,123 @@
+<template>
+    <li>
+      <label>
+        <!--      这里勾选和取消勾选可以使用change和click作为事件处理-->
+        <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)"/>
+        <!--v-model数据的双向绑定，checkbox使用v-model来双向绑定其是否被勾选,也可以实现效果但不推荐(因为其实修改了props中的数据)-->
+        <!--这里修改了从List修改过来的props,这里的不允许改是浅层次，就是如果props是一个对象则这个修改这个对象的某一个属性vue是放行的-->
+        <!-- <input type="checkbox" v-model="todo.done"/>-->
+        <span v-show="!todo.isEdit">{{ todo.title }}</span>
+        <input v-show="todo.isEdit" ref="inputTitle" type="text" :value="todo.title" @blur="handleBlur(todo,$event)">
+      </label>
+      <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+      <button class="btn btn-edit" v-show="!todo.isEdit" style="margin-right: 5px" @click="handleEdit(todo)">编辑
+      </button>
+    </li>
+</template>
+<script>
+import pubsub from "pubsub-js";
+
+export default {
+  name: 'Item',
+  //声明接收todo
+  props: ['todo'],
+  data() {
+    return {}
+  },
+  methods: {
+    handleCheck(id) {
+      // this.checkTodo(id);
+      // 触发事件、发数据
+      this.$bus.$emit('checkTodo', id)
+    },
+    handleDelete(id) {
+      if (confirm(`确定删除编号为${id}的todo吗`)) {
+        // console.log(id);
+        // this.deleteTodo(id);
+        //   触发事件、发数据
+        // this.$bus.$emit('deleteTodo',id)
+
+        pubsub.publish('deleteTodo', id)
+      }
+    },
+    handleEdit(todo) {
+      //  判断有没有这个属性
+      if (todo.hasOwnProperty('isEdit')) {
+        todo.isEdit = true
+      } else {
+        this.$set(todo, 'isEdit', true)
+      }
+      // console.log(todo)
+
+      // 获取焦点 //没有获取到
+      //  执行完了handleEdit方法之后，当走这句话的时候，input还没渲染到页面上
+      //  加个延时就可以
+      // this.$refs.inputTitle.focus()
+
+      // 下一轮
+      this.$nextTick(() => {
+        //   Dom节点更新之后，才会生效
+        this.$refs.inputTitle.focus()
+      })
+    },
+    // 失去焦点回调
+    handleBlur(todo, e) {
+      todo.isEdit = false
+
+      if (!e.target.value.trim()) {
+        return alert("输入不能为空")
+      }
+      this.$bus.$emit('updateTodo', todo.id, e.target.value)
+    }
+
+  },
+  components: {}
+}
+</script>
+<style scoped>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+li:hover {
+  background: #ddd;
+}
+
+li:hover button {
+  display: block;
+}
+
+
+
+</style>
---
+++ Vue2/vue_test/18_src_TodoList_动画/components/List.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul class="todo-main">
+    <transition-group name="todo" appear>
+      <Item
+          v-for="todoObj in todos"
+          :key="todoObj.id"
+          :todo="todoObj"
+      />
+    </transition-group>
+  </ul>
+</template>
+<script>
+import Item from "./Item.vue";
+
+export default {
+  name: 'List',
+  data() {
+    return {}
+  },
+  props: ['todos'],
+  methods: {},
+  components: {Item}
+}
+</script>
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+
+@keyframes disney {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
+/**
+  vue的设计
+  默认是 v
+.v-enter-active {
+  animation: disney 1s;
+}
+ */
+.todo-enter-active {
+  animation: disney 1s;
+}
+
+.todo-leave-active {
+  animation: disney 1s reverse;
+}
+</style>
