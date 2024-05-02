34a8b8e13cd540f9c53d70f871ad9a9c4a474eeb
Jiligu <2497050447@qq.com>
2024-05-02 23:37:28 +0800

    feat(19_src_配置代理服务器): 使用Axios

---
+++ Vue2/vue_test/19_src_配置代理服务器/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+
+import pubsub from "pubsub-js";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import List from "./components/List.vue";
+export default {
+
+  name: 'App',
+  components: {List, Footer, Header},
+  data() {
+    return {}
+  },
+}
+</script>
+<style>
+</style>
