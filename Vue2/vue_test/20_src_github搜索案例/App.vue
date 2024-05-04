47be3e051539a8aae156671f1b596eda0f959edf
Jiligu <2497050447@qq.com>
2024-05-04 22:54:50 +0800

    feat(20_src_github搜索案例): 通过axios发送请求拿到github用户
    使用全局事件总线处理数据

---
+++ Vue2/vue_test/20_src_github搜索案例/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div className="container">
+    <Search></Search>
+    <List/>
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search";
+import List from "./components/List";
+
+export default {
+  name: "App",
+  components: {
+    Search,
+    List
+  }
+}
+</script>
+<style lang="css">
+</style>
