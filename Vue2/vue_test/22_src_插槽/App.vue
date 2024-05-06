51edd80277836e4779fa4afb1563001390b50d98
Jiligu <2497050447@qq.com>
2024-05-06 23:08:43 +0800

    feat(22_src_插槽): 三种插槽使用方式
    1、默认插槽
    2、具名插槽（别名）
    3、作用域插槽（可以传值）

---
+++ Vue2/vue_test/22_src_插槽/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <Category title="美食" :listData="foods">
+      <img slot="center" src="https://static001.infoq.cn/resource/image/c4/01/c4251ee38c2039602d69cac1d3ab9d01.jpg"
+           alt="">
+      <a slot="footer" href="http://localhost:8080">更多美食</a>
+    </Category>
+    <Category title="游戏" :listData="games">
+      <ul slot="center">
+        <li v-for="(item,index) in games" :key="index">{{ item }}</li>
+      </ul>
+    </Category>
+    <Category title="电影" :listData="films">
+      <video controls></video>
+      <h4 slot="footer">欢迎前来观看</h4>
+      <!--      v-slot:footer，最新的插槽写法，只能写在template上-->
+      <!--      <template v-slot:footer></template>-->
+    </Category>
+
+    <!--    拿到传过来的数据-->
+    <Category>
+      <template slot="test" scope="disney">
+        {{disney.test}}}
+        <ul>
+          <li v-for="(item,index) in disney.test" :key="index">{{ item }}</li>
+        </ul>
+      </template>
+    </Category>
+    <Category>
+      <template slot="test" scope="tests">
+        <ol>
+          <li v-for="(item,index) in tests.test" :key="index">{{ item }}</li>
+        </ol>
+      </template>
+    </Category>
+
+  </div>
+</template>
+
+<script>
+
+import Category from "./components/Category.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      foods: ['火锅', '烧烤', '小龙虾', '牛排'],
+      games: ['红色警戒', '穿越火线', '劲舞团', '超级玛丽'],
+      films: ['当幸福来敲门', '教父', '《拆弹专家》', '《你好，李焕英》']
+    }
+  },
+  components: {
+    Category
+  }
+}
+</script>
+<style lang="css">
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ Vue2/vue_test/22_src_插槽/components/Category.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="category">
+    <h3>{{ title }}分类</h3>
+    <!--    <ul v-show="title !== '美食' ">-->
+    <!--      <li v-for="(item,index) in listData" :key="index">{{ item }}</li>-->
+    <!--    </ul>-->
+    <!--    <img v-show=" title === '美食' " src="https://static001.infoq.cn/resource/image/c4/01/c4251ee38c2039602d69cac1d3ab9d01.jpg" alt="">-->
+    <!--     <video></video>-->
+
+    <!--    插槽、定一个插槽（等着组件的使用者进行填充）-->
+    <!--    默认插槽-->
+    <slot name="center">如果没有使用，就会出现插槽的默认值</slot>
+    <slot name="footer">如果没有使用，就会出现插槽的默认值</slot>
+
+    <!--    把tests传一个数据-->
+    <slot :test="tests" name="test">如果没有使用，就会出现插槽的默认值</slot>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'Category',
+  data() {
+    return {
+      tests: [
+        '1', '2', '3'
+      ]
+    }
+  },
+  methods: {},
+  components: {},
+  props: ['title']
+}
+</script>
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+
+img {
+  width: 100%;
+}
+
+video {
+  width: 100%;
+}
+
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+</style>
