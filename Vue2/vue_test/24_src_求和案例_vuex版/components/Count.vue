8a74720f301525115553377b9c619a7b35d84860
Jiligu <2497050447@qq.com>
2024-05-07 17:49:25 +0800

    feat(24_src_求和案例_vuex版): 使用vuex
    使用vuex来操作数据
    组件中读取vuex中的数据：$store.state.sum
    组件中修改vuex中的数据：$store.dispatch('action中的方法名',数据) 或 $store.commit('mutations中的方法名',数据)

---
+++ Vue2/vue_test/24_src_求和案例_vuex版/components/Count.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>当前求和为：{{ $store.state.sum }}</h1>
+    <select v-model.number="n">
+      <option :value="1">1</option>
+      <option :value="2">2</option>
+      <option :value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Count',
+  data() {
+    return {
+      n: 1  //  用户选择的数字
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.dispatch("jia", this.n);
+      // console.log(this)
+    },
+    decrement() {
+      // 跳过actions，没有业务逻辑直接跳过actions
+      this.$store.commit("JIAN", this.n);
+    },
+    incrementOdd() {
+      // if (this.$store.state.sum % 2) {
+      //   this.$store.dispatch("jia", this.n);
+      // }
+      this.$store.dispatch("jiaOdd", this.n);
+    },
+    incrementWait() {
+      // setTimeout(() => {
+      //   this.$store.dispatch("jia", this.n);
+      // }, 500)
+      this.$store.dispatch("jiaWait", this.n);
+    },
+  },
+  components: {}
+}
+</script>
+<style>
+button {
+  margin-left: 5px;
+}
+</style>
---
+++ Vue2/vue_test/24_src_求和案例_vuex版/store/index.js
@@ -0,0 +1,54 @@
+//  创建Store,Vuex
+
+// 引入Vuex
+import Vuex from 'vuex'
+// 引入vue，如果卸载main.js，就会先扫描了所有的import才去vue.use，就会报错
+import Vue from "vue";
+// 准备action，用于相应组件中的动作
+const actions = {
+    jia: function (context, val) {
+        // console.log("执行jia")
+        // console.log(context)
+        // console.log(val)
+        context.commit('JIA', val)
+    },
+    // 没有业务逻辑可以跳过、      this.$store.commit("JIAN", this.n);
+    // jian(context, val) {
+    //     context.commit('JIAN', val)
+    // },
+    jiaOdd(context, val) {
+        if (context.state.sum % 2) {
+            context.commit('JIA', val)
+        }
+    },
+    jiaWait(context, val) {
+        setTimeout(() => {
+            context.commit('JIA', val)
+        }, 500)
+    }
+}
+// 准备mutations 用于操作数据
+const mutations = {
+    JIA(state, val) {
+        // console.log("mutations被调用",state,val)
+        state.sum += val
+    },
+    JIAN(state, val) {
+        state.sum -= val
+    }
+}
+
+//  准备state 用于存储数据
+const state = {
+    sum: 0, //  当前的和
+}
+
+Vue.use(Vuex)
+// 创建store
+const store = new Vuex.Store({
+    actions: actions,
+    mutations: mutations,
+    // 简写
+    state
+})
+export default store
