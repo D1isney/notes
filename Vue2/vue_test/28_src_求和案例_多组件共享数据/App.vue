c570fa3b846002b452869ac7cdd92e67d007e696
Jiligu <2497050447@qq.com>
2024-05-08 22:29:48 +0800

    feat(28_src_求和案例_多组件共享数据): 使用vuex来进行多组件数据的使用

---
+++ Vue2/vue_test/28_src_求和案例_多组件共享数据/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Count></Count>
+    <hr>
+    <Person></Person>
+  </div>
+</template>
+
+<script>
+import Count from './components/Count.vue'
+import Person from "./components/Person.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+    }
+  },
+  components: {
+    Person,
+    Count
+  }
+}
+</script>
+<style lang="css">
+</style>
---
+++ Vue2/vue_test/28_src_求和案例_多组件共享数据/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>求和：{{ sum }}</h1>
+    <h1>人员列表</h1>
+    <input type="text" placeholder="请输入名字" v-model="name">
+    <button @click="add">添加</button>
+    <ul>
+      <li v-for="p in personList" :key="p.id">
+        {{ p.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import {mapState} from "vuex";
+import {nanoid} from "nanoid";
+
+export default {
+  name: 'Person',
+  data() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    add() {
+      let personObj = {id: nanoid(), name: this.name}
+      this.$store.commit('ADD_PERSON', personObj)
+      this.name = '';
+    }
+  },
+  components: {},
+  computed: {
+    personList() {
+      return this.$store.state.personList;
+    },
+    // ...mapState(['personList']),
+    sum(){
+      return this.$store.state.sum;
+    }
+  }
+}
+</script>
+<style>
+
+</style>
