44832ec44857911a2c3c8bc2f7c531223d758949
Jiligu <2497050447@qq.com>
2024-05-10 23:23:59 +0800

    feat(33_src_命名路由): name
    给路由命名
    简化跳转：:to="{name:'about'}"

---
+++ Vue2/vue_test/33_src_命名路由/pages/Detail.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li>
+<!--      ID：{{ $route.query.id }}-->
+      ID：{{ $route.params.id }}
+      <br>
+<!--      Title：{{ $route.query.title }}-->
+      Title：{{ $route.params.title }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name:'Detail',
+  data(){
+    return{}
+  },
+  methods:{},
+  components:{},
+  mounted() {
+    console.log(this.$route)
+  }
+}
+</script>
+<style>
+
+</style>
