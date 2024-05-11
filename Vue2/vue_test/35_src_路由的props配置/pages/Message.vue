8066fd9c990ed83a25bef7466aa2f8bbc8d5b75d
Jiligu <2497050447@qq.com>
2024-05-11 12:17:59 +0800

    feat(35_src_路由的props配置): 路由的props配置
    {
            name: 'detail',
            // path: 'detail/:id/:title',
            path: 'detail',
            component: Detail,
            // props的第一种写法、传给Detail组件
            // props: {
            //     a: 1,
            //     b: 'hello'
            // }
    
            //     props的第二种写法，值为布尔值，若布尔值微针，就会把该组件收到的所有的params参数，以props的形式传给Detail组件
            // props: true
    
            //     props的第三种写法，该函数返回的对象中每一组key-value都会通过props传给Detail组件
            props($route){
                return {id:$route.query.id,title:$route.query.title}
            }
        }

---
+++ Vue2/vue_test/35_src_路由的props配置/pages/Message.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item,index) in messageList" :key="item.id">
+        <!--        跳转路由并携带query参数，to的字符串写法-->
+        <!--        <router-link :to="`/home/message/detail?id=${item.id}&title=${item.title}`">{{ item.title }}</router-link>-->
+
+        <!--        跳转路由并携带query参数，to的对象写法-->
+        <router-link :to="{
+                  name:'detail',
+                  query:{
+                    id: item.id,
+                    title: item.title
+                  }
+                }">
+          {{ item.title }}
+        </router-link>
+
+        <!--        <router-link :to="`/home/message/detail/${item.id}/${item.title}`">{{ item.title }}</router-link>-->
+      </li>
+    </ul>
+    <hr>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data() {
+    return {
+      messageList: [
+        {id: '001', title: '消息01'},
+        {id: '002', title: '消息02'},
+        {id: '003', title: '消息03'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
