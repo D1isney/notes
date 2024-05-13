8c84e488bdf4d7d592f23c7e439dcce869fd64a3
Jiligu <2497050447@qq.com>
2024-05-13 17:11:13 +0800

    feat(39_src_两个新的生命周期钩子): 两个新的生命周期钩子
    作用：路由组件所独有的两个钩子，用于捕获路由组件的激活状态
    具体名字：
    1、activated：路由组件被激活时触发
    2、deactivated：路由组件失活时触发
    // 激活
      activated() {
        console.log("News被激活了")
        this.timer =  setInterval(() => {
          this.opacity -= 0.001
          if (this.opacity <= 0) this.opacity = 11
    
        })
      },
      // 失活
      deactivated() {
        console.log("News被失活了")
        clearInterval(this.timer)
      }

---
+++ Vue2/vue_test/39_src_两个新的生命周期钩子/pages/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <div>
+      <ul class="nav nav-tabs">
+        <li>
+          <router-link replace class="list-group-item" active-class="active" to="/home/news">News</router-link>
+        </li>
+        <li>
+          <router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+        </li>
+      </ul>
+      <!--      指定缓存谁 不写就是都缓存 定义的是组件的名-->
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  // mounted() {
+  //   console.log('Home组件挂载完毕', this);
+  //   window.homeRoute = this.$route;
+  //   window.homeRouter = this.$router;
+  // },
+  // // beforeDestroy() {
+  // //   console.log('Home组件将要被销毁');
+  // // }
+  data() {
+    return{
+
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+</style>
