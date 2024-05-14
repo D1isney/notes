584a0ff5e05d4b8bd1a0dd6615f4d58a81f6684e
Jiligu <2497050447@qq.com>
2024-05-14 23:43:03 +0800

    feat(40_src_全局路由守卫): 对路由进行权限控制
    全局路由、独享守卫、组件内守卫

---
+++ Vue2/vue_test/40_src_全局路由守卫/pages/Detail.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li>
+<!--      ID：{{ $route.query.id }}-->
+<!--      ID：{{ a }}-->
+      ID：{{ id }}
+      <br>
+<!--      Title：{{ $route.query.title }}-->
+      Title：{{ title }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'Detail',
+  data() {
+    return {}
+  },
+  methods: {},
+  components: {},
+  mounted() {
+    // console.log(this.$route)
+  },
+  props: ['id', 'title']
+}
+</script>
+<style>
+
+</style>
