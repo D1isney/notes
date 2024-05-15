36ff392ce3d30e65570d25214dd316049b8c0994
Jiligu <2497050447@qq.com>
2024-05-15 23:45:12 +0800

    feat(43_src_history模式与hash模式): history与hash的关系
    history：localhost:8080，后面的api就不会转为哈希，当路径出现路由路径的时候，刷新，会把当前路径地址发送给后端，会报错，可以使用node.js的插件或者使用java的一些类库处理
    hash：localhost:8080/#/，后面的api会转为哈希，路由路径刷新不会报错

---
+++ Vue2/vue_test/43_src_history模式与hash模式/router/index.js
@@ -0,0 +1,97 @@
+import VueRouter from "vue-router";
+import About from "../pages/About";
+import Home from '../pages/Home';
+import News from "../pages/News";
+import Message from "../pages/Message";
+import Detail from "../pages/Detail.vue";
+
+//创建并默认暴露一个路由器
+const router = new VueRouter({
+    // mode:'history',
+    mode: 'hash',
+    routes: [
+        {
+            path: '/about',
+            component: About,
+            meta: {
+                title: 'about',
+                isAuth: true
+            }
+        },
+        {
+            path: '/home',
+            component: Home,
+            meta: {
+                title: 'home',
+            },
+            children: [
+                {
+                    path: 'news',
+                    component: News,
+                    meta: {
+                        isAuth: true,
+                        title: 'news',
+                    }
+                },
+                {
+                    path: 'message',
+                    component: Message,
+                    children: [
+                        {
+                            name: 'detail',
+                            // path: 'detail/:id/:title',
+                            path: 'detail',
+                            component: Detail,
+                            // props的第一种写法、传给Detail组件
+                            // props: {
+                            //     a: 1,
+                            //     b: 'hello'
+                            // }
+
+                            //     props的第二种写法，值为布尔值，若布尔值微针，就会把该组件收到的所有的params参数，以props的形式传给Detail组件
+                            // props: true
+
+                            //     props的第三种写法，
+                            props($route) {
+                                return {id: $route.query.id, title: $route.query.title}
+                            }
+                        }
+                    ],
+                    meta: {
+                        title: 'message',
+                        isAuth: true
+                    }
+                }
+            ]
+        }
+    ]
+});
+//
+// // 全局前置路由守卫 -- 初始化的时候被调用、每次路由切换之前被调用
+// router.beforeEach((to, from, next) => {
+//     // console.log(to,from)
+//     //     title名
+//     //     document.title = to.meta.title
+//     // document.title = to.meta["title"]
+//
+// //     next 放行
+// //     判断是否需要鉴权
+//     if (to.meta["isAuth"]) {
+//         if (localStorage.getItem("name") === 'Disney') {
+//             // document.title = to.meta["title"]
+//             next()
+//         } else {
+//             alert('权限不对')
+//         }
+//     } else {
+//         // document.title = to.meta["title"]
+//         next()
+//     }
+// })
+//
+// // 全局后置路由守卫
+// router.afterEach((to, from) => {
+//     document.title = to.meta["title"]
+//     console.log("后置路由：", to, from)
+// })
+export default router
