cd1e3741f5704b1e6925d9068f27fa3ee0488cd1
Jiligu <2497050447@qq.com>
2024-05-19 14:06:49 +0800

    feat(02_src_初识setup): 初识setup
    尽量不要Vue2跟Vue3一起写，Vue2能读取到Vue3的数据，但是Vue3不能读取Vue2的数据
    当两个数据名一样时，优先使用的是setup的

---
+++ vue3/02_src_初识setup/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Vue3</h1>
+  <h2>name:{{ name }}</h2>
+  <h2>age:{{ age }}</h2>
+  <h2>sex:{{ sex }}</h2>
+  <button @click="sayHello">+++---</button>
+</template>
+
+<script>
+
+// import {h} from "vue";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      sex: '男',
+    }
+  },
+  methods: {
+    sayVue() {
+      console.log(this.name + " + ", this.sex)
+    }
+  },
+  // async setup() {
+  setup() {
+    let name = '张三'
+    let age = 20
+
+    function sayHello() {
+      alert("HelloVue3")
+    }
+
+    //  返回一个对象
+    return {
+      name, age,
+      sayHello
+    }
+
+    //  渲染函数
+    // return () => {
+    //   return h('div','123')
+    // }
+  }
+}
+</script>
+
+<style>
+</style>
