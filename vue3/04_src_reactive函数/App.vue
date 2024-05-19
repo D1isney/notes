94947f1f685b41a61fce49feb2c3f40224a72c31
Jiligu <2497050447@qq.com>
2024-05-19 15:10:38 +0800

    feat(04_src_reactive函数): 定义一个对象类型的响应式数据（基本类型不要用它，用ref函数）
    语法：const 代理对象 = reactive（源对象）接收一个对象（或数据），返回一个代理对象（proxy对象）
    reactive定义的响应式数据是“深层次的”
    内部基于ES6的Proxy实现，通过代理对象操作源对象内部数据进行操作

---
+++ vue3/04_src_reactive函数/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <h1>Vue3</h1>
+  <h2>name:{{ name }}</h2>
+  <h2>age:{{ age }}</h2>
+  <h3>type:{{ job.type }}</h3>
+  <h3>salary:{{ job.salary }}</h3>
+  <h3>c:{{ student.a.b.c }}</h3>
+  <h3>arr{{ arr }}</h3>
+  <button @click="changeInfo">修改人的信息</button>
+</template>
+
+<script>
+import {ref, reactive} from 'vue'
+
+export default {
+  name: 'App',
+
+  setup() {
+    let name = ref('张三')
+    let age = ref(20)
+    let job = ref({
+      type: '前端',
+      salary: '30k'
+    })
+    let student = reactive({
+      name: 'Hello Vue3',
+      msg: 'Test',
+      a: {
+        b: {
+          c: 666
+        }
+      }
+    })
+
+    let arr = reactive([
+        '1','2','3'
+    ])
+
+    function changeInfo() {
+      console.log(name, age)
+      name.value = 'Vue3';
+      age.value = 23
+      job.value.type = '后端'
+      job.value.salary = '50k'
+      student.a.b.c = 999
+      arr[0] = '2'
+      console.log(student)
+    }
+
+
+    //  返回一个对象
+    return {
+      name, age, job, student, arr,
+      changeInfo
+    }
+  }
+}
+</script>
+
+<style>
+</style>
