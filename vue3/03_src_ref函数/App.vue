5d19c70ef0e201a4e096e36dd52c32b63a29f462
Jiligu <2497050447@qq.com>
2024-05-19 14:46:01 +0800

    feat(03_src_ref函数): 定义一个响应式的数据
    语法：const xxx = ref(xxx)
    基本类型的数据：响应式依然是靠Object.defineProperty()的get与set完成的
    对象类型的数据：内部“求助”了Vue3.0中的一个新函数----reactive函数

---
+++ vue3/03_src_ref函数/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>Vue3</h1>
+  <h2>name:{{ name }}</h2>
+  <h2>age:{{ age }}</h2>
+  <h3>type:{{job.type}}</h3>
+  <h3>salary:{{job.salary}}</h3>
+  <button @click="changeInfo">修改人的信息</button>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'App',
+
+  setup() {
+    let name = ref('张三')
+    let age = ref(20)
+    let job = ref({
+      type:'前端',
+      salary:'30k'
+    })
+    function changeInfo() {
+      console.log(name,age)
+      name.value = 'Vue3';
+      age.value = 23
+      job.value.type = '后端'
+      job.value.salary = '50k'
+    }
+
+
+    //  返回一个对象
+    return {
+      name, age,job,
+      changeInfo
+    }
+  }
+}
+</script>
+
+<style>
+</style>
