24c9618510373dffeb56529ac9579ac8e5a28693
Jiligu <2497050447@qq.com>
2024-05-21 22:26:31 +0800

    Hello Vue3
    实训周（写项目中.....）

--- vue3/vue.config.js
+++ vue3/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    index: {
+      entry: '05_src_Vue3的响应式原理/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Index Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    subpage: '05_src_Vue3的响应式原理/main.js'
+  },
+  lintOnSave:false /*关闭语法检查*/
+}
