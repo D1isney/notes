543c537d692b49312937c72952f2d02de94ce3ab
Jiligu <2497050447@qq.com>
2024-05-27 23:17:04 +0800

    feat(07_src_computed计算属性): 计算属性
    在Vue2.x中computed配置功能一致

---
+++ vue3/07_src_computed计算属性/components/Demo.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>个人信息</h1>
+  姓：<input type="text" v-model="person.firstName">
+  <br>
+  名：<input type="text" v-model="person.lastName">
+  <br>
+  <span>全名：{{ fullName }}</span>
+  <br>
+  全名：<input type="text" v-model="fullName">
+</template>
+<script>
+import {reactive, computed} from "vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Demo',
+  setup() {
+    let person = reactive({
+      firstName: "张",
+      lastName: "三"
+    })
+    // 计算属性，简写（没有修改的情况）
+    // let fullName = computed(() => {
+    //   return person.firstName + "-" + person.lastName;
+    // })
+    // 计算属性，简写（没有修改的情况）
+    let fullName = computed({
+      get() {
+        return person.firstName + "-" + person.lastName;
+      },
+      set(value) {
+        let name = value.split("-");
+        person.firstName = name[0];
+        person.lastName = name[1];
+      }
+    })
+
+    return {
+      person, fullName
+    }
+  }
+}
+</script>
+<style>
+
+</style>
