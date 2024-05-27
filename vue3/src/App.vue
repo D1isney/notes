c594429feb03865781fe66ad2d552c7d677c5ed2
Jiligu <2497050447@qq.com>
2024-05-27 23:17:17 +0800

    feat(src):

---
+++ vue3/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Demo msg="Hello" @hello="showHelloMsg"></Demo>
+  </div>
+</template>
+
+<script>
+import Demo from './components/Demo.vue'
+export default {
+  name: 'App',
+  setup() {
+    function showHelloMsg(){
+      console.log('hello')
+    }
+    return{
+      showHelloMsg
+    }
+  },
+  components: {Demo}
+}
+</script>
+
+<style>
+</style>
---
+++ vue3/src/components/Demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="test">btn</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Demo',
+  setup(props,context){
+    console.log(context)
+    console.log('setup'+context.attrs)
+    function test(){
+      context.emit('hello')
+    }
+    return{
+      test
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate')
+  }
+}
+</script>
+<style>
+
+</style>
