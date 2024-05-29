3ed7afe03738188d645b80e4bbc963558bf498a2
Jiligu <2497050447@qq.com>
2024-05-29 15:57:27 +0800

    feat(10_src_watchEffect函数): watchEffect函数
    watch的套路是：既要指明监视的属性，也要指明监视的回调
    watchEffect的套路是：不用指明监视哪个属性，监视的回调中用到哪个属性，那就监视哪个属性。
    watchEffect有点像computed：
        但computed注重的计算出来的值（回调函数的返回值），所以必须要写返回值。
        而watchEffect更注重的是过程（回调函数的函数体），所以不用写返回值。
    
    watchEffect(() => {
      const x1 = sum.value;
      console.log(x1)
      console.log("watchEffect")
    })

---
+++ vue3/10_src_watchEffect函数/components/Demo.vue
@@ -0,0 +1,60 @@
+<template>
+  <h2>当前求和为：{{ sum }}</h2>
+  <button @click="sum++">点我+1</button>
+  <hr>
+  <h2>当前的信息为：{{ msg }}</h2>
+  <button @click="msg+='!'">点我+！</button>
+  <hr>
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年龄：{{ person.age }}</h2>
+  <h2>薪资：{{ person.job.j1.salary }}</h2>
+  <button @click="person.name += '~'">修改姓名</button>
+  <button @click="person.age ++">增长年龄</button>
+</template>
+<script>
+import {reactive, ref, watch, watchEffect} from "vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Demo',
+  setup() {
+    let sum = ref(0)
+    let msg = ref('Hello')
+
+    let person = reactive({
+      name: '张三',
+      age: 18,
+      job: {
+        j1: {
+          salary: 20
+        },
+        j2: {
+          salary: 30
+        }
+      }
+    })
+
+    // watch(sum, (newValue, oldValue) => {
+    //   console.log(newValue, oldValue)
+    // },{
+    //   immediate : true
+    // })
+
+    watchEffect(() => {
+      const x1 = sum.value;
+      console.log(x1)
+      console.log("watchEffect")
+    })
+
+
+    return {
+      sum, msg
+      , person
+    }
+
+  }
+}
+</script>
+<style>
+
+</style>
