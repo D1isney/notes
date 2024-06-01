39e7a3a7dd5250b83551cd7b92285586b3067ec8
Jiligu <2497050447@qq.com>
2024-06-01 22:42:37 +0800

    feat(19_src_响应式数据的判断): 响应式数据的判断
    isRef：检查一个值是否为一个ref对象
    isReactive：检查一个对象是否由reactive创建的响应式代理
    isReadonly：检查一个对象是否由readonly创建的只读代理
    isProxy：检查一个对象是否由reactive或者readonly方法创建的代理

---
+++ vue3/19_src_响应式数据的判断/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app">
+    <h3>APP组件</h3>
+  </div>
+</template>
+<script>
+
+
+import {isProxy, isReactive, isReadonly, isRef, reactive, readonly, ref} from "vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'App',
+  setup: function () {
+    let car = reactive({
+      name: '奔驰',
+      price: '400w'
+    })
+    let car2 = readonly(car)
+    let sum = ref(0)
+
+    console.log(isRef(sum))
+    console.log(isReactive(car))
+    console.log(isReadonly(car2))
+    console.log(isProxy(sum))
+    console.log(isProxy(car2))
+
+    return {
+      car, car2, sum
+    }
+  },
+  components: {}
+}
+</script>
+<style>
+.app {
+  background-color: #cccccc;
+  padding: 10px;
+}
+</style>
