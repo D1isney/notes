b0fcece1a83045353df5ab80a767b651ef0b1784
Jiligu <2497050447@qq.com>
2024-06-02 23:06:27 +0800

    feat(21_src_Suspense): 拉开序幕的setup
    1、理解：vue3.0中一个新的配置项，值为一个函数
    2、setup是所有Composition API（组合API）“表演的舞台”
    3、组件中所有用到：数据方法等等，均要配置在setup中
    4、setup函数的两种返回值：
        1、若返回一个对象，则对象中的属性、方法，在模版中均可以直接使用
        2、若返回一个渲染函数：则可以自定义渲染内容
    5、注意点：
        1、尽量不要与Vue2.x配置混用
            1.Vue2.x配置（data、methos、computed...）中可以访问到setup中的属性、方法
            2.但在setup中不能访问到Vue2.x配置（data、methos、computed）
            3.如果有重名，setup有限
        2、setup不能是一个async函数，因为返回值不再是return对象，而是promise，模版看不到retrn对象中的属性。（后期也可以返回一个Promise实例，但需要Suspense和异步组件的配合）

---
+++ vue3/21_src_Suspense/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <h3>APP组件</h3>
+    <Suspense>
+      <template v-slot:default>
+        <Child></Child>
+      </template>
+      <template v-slot:fallback>
+        <!--        退路-->
+        <!--        上面那个组件不行了，就使用这个-->
+        <h3>稍等....加载中</h3>
+      </template>
+    </Suspense>
+  </div>
+</template>
+<script>
+//  静态引入
+// import Child from "./components/Child.vue";
+import {defineAsyncComponent} from "vue";
+//  动态引入、异步引入
+const Child = defineAsyncComponent(() => {
+  return import("./components/Child")
+})
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'App',
+  components: {
+    Child
+  }
+}
+</script>
+<style>
+.app {
+  background-color: #cccccc;
+  padding: 10px;
+}
+</style>
---
+++ vue3/21_src_Suspense/components/Child.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="child">
+    <h3>Child组件</h3>
+    {{ sum }}
+  </div>
+</template>
+<script>
+
+
+import {ref} from "vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Child',
+  async setup() {
+    let sum = ref(0)
+    let p = new Promise((resolve, reject) => {
+      console.log(reject)
+      setTimeout(() => {
+        resolve({sum})
+      }, 1000)
+    })
+    return await p
+    // return{
+    //   sum
+    // }
+  }
+}
+</script>
+<style>
+.child {
+  background-color: skyblue;
+  padding: 10px;
+}
+</style>
