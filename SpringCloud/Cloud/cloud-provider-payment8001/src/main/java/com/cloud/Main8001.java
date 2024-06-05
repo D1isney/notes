5c9778aaa4f7172891ec2bee99c72420a278e021
Jiligu <2497050447@qq.com>
2024-06-05 23:20:55 +0800

    feat(cloud-provider-payment8001): 上述模块还有哪些问题 时间格式问题? Java如何设计API接口实现统一格式返回？ 全局异常接入返回的标准格式?
    Signed-off-by: Jiligu <2497050447@qq.com>

---
+++ SpringCloud/Cloud/cloud-provider-payment8001/src/main/java/com/cloud/Main8001.java
@@ -0,0 +1,17 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+//  扫描所有mapper
+@MapperScan("com.cloud.mapper")
+//import tk.mybatis.spring.annotation.MapperScan;
+public class Main8001
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Main8001.class,args);
+    }
+}
