db085287e0a1e6be84bec7f8fe4f68ca76ff35f8
Jiligu <2497050447@qq.com>
2024-06-10 23:07:35 +0800

    feat(Spring-Cloud-LoadBalancer): 负载均衡
    Spring Cloud LoadBalancer是由Spring
    Cloud官方提供的一个开源的、简单易用的客户端负载均衡，它包含在Spring
    Cloud-commons中用它来替换了以前的Ribbon组件。相比较于Ribbon，Spring Cloud
    LoadBalancer不仅能够支持RestTemplate。还支持，WebClient（WebClient是Spring Web
    Flux中提供的功能，可以实现响应式异步请求）

--- SpringCloud/Cloud/cloud-consumer-order80/src/main/java/com/cloud/controller/OrderController.java
+++ SpringCloud/Cloud/cloud-consumer-order80/src/main/java/com/cloud/controller/OrderController.java
@@ -3,14 +3,18 @@ package com.cloud.controller;
 import com.cloud.entities.PayDTO;
 import com.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
     //    public static final String PaymentSrv_URL = "http://localhost:8001";
 //    注册中心叫什么，这里就叫什么
-    public static final String PaymentSrv_URL = "http://cloud-payment-service8001";
+    public static final String PaymentSrv_URL = "http://cloud-payment-service";
 
     @Resource
     private RestTemplate restTemplate;
@@ -38,6 +42,26 @@ public class OrderController {
     public ResultData<?> getPayInfo(@PathVariable("id") Integer id) {
         return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/" + id, ResultData.class, id);
     }
+    @Resource
+    private DiscoveryClient discoveryClient;
 
+    @GetMapping(value = "/consumer/pay/get/info")
+    private String getInfoByConsul() {
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/info", String.class);
+    }
+
+    @GetMapping("/consumer/discovery")
+    public String discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+        System.out.println("================");
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t" + element.getUri());
+        }
+        return instances.get(0).getServiceId() + ":" + instances.get(0).getPort();
+    }
 
 }
--- SpringCloud/Cloud/cloud-provider-payment8001/src/main/java/com/cloud/Main8001.java
+++ SpringCloud/Cloud/cloud-provider-payment8001/src/main/java/com/cloud/Main8001.java
@@ -3,6 +3,7 @@ package com.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
@@ -10,6 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.cloud.mapper")
 //import tk.mybatis.spring.annotation.MapperScan;
 @EnableDiscoveryClient
+@RefreshScope   //  动态刷新
 public class Main8001
 {
     public static void main(String[] args) {
