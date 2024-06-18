bdaecde1b402867a215db9b29dc6a1f65e9573bd
Jiligu <2497050447@qq.com>
2024-06-18 23:22:07 +0800

    feat(Nacos服务注册和配置中心): Nacos
    一个更易于构建云原生应用的动态服务发现、配置管理和服务管理平台。
    
    Nacos就是注册中心 + 配置中心的组合
    
    Nacos = Eureka
    + Config + Bus
    
    Nacos = Spring Cloud Consul

---
+++ SpringCloud/Cloud/cloudalibaba-consumer-nacos-order83/src/main/java/com/cloud/controller/OrderNaCosController.java
@@ -0,0 +1,23 @@
+package com.cloud.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderNaCosController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping("/consumer/pay/nacos/{id}")
+    public String consumer(@PathVariable("id") Integer id) {
+        return restTemplate.getForObject(serverURL + "/pay/nacos/" + id, String.class) + "\t" + "  我是OrderNacosController83调用者";
+    }
+}
---
+++ SpringCloud/Cloud/cloudalibaba-provider-payment9001/src/main/java/com/cloud/controller/PayAlibabaController.java
@@ -0,0 +1,20 @@
+package com.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PayAlibabaController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/pay/nacos/{id}")
+    public String getPayInfo(@PathVariable("id") Integer id) {
+        return "nacos registry,ServerPort：" + serverPort + "\t" + "Id：" + id;
+    }
+
+}
