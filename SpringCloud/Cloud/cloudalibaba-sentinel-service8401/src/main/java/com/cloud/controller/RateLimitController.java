2a9c7fb01135000d6ff00bc2587d5bdcf44c2381
Jiligu <2497050447@qq.com>
2024-06-20 23:28:45 +0800

    feat(Alibaba Sentinel): Sentinel
    随着微服务的流行，服务和服务之间的稳定性变得越来越重要。Sentinel是面向分布式、多语言异构化服务架构的治理组件，只要以流量为切入点，从流量路由、流量控制、流量整形、熔断降级、系统自适应过载保护、热点流量防护等多个维度来帮助开发者保障微服务的稳定性。
    
    Sentinel分为两个部分：
    
    -
    核心库（Java客户端）不依赖任何框架/库，能够运行所有Java运行时环境，同时对Dubbo
    / Spring
    Cloud等框架也有较好的支持。
    -
    控制台（Dashboard）基于Spring
    Boot开发，打包后可以直接运行，不需要额外的Tomcat等应用容器。

--- SpringCloud/Cloud/cloudalibaba-sentinel-service8401/src/main/java/com/cloud/controller/RateLimitController.java
+++ SpringCloud/Cloud/cloudalibaba-sentinel-service8401/src/main/java/com/cloud/controller/RateLimitController.java
@@ -1,7 +1,11 @@
 package com.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +16,49 @@ public class RateLimitController {
     public String byUrl() {
         return "按rest地址限流测试";
     }
+
+    @GetMapping(value = "/rateLimit/byResource")
+    @SentinelResource(value = "byResourceSentinelResource", blockHandler = "handlerBlockHandler")
+    public String byResource() {
+        return "按照资源名称SentinelResource限流测试";
+    }
+
+    public String handlerBlockHandler(BlockException blockException) {
+        return "服务不可用触发了@SentinelResource";
+    }
+
+    @GetMapping("/rateLimit/doAction/{p1}")
+    @SentinelResource(value = "doActionSentinelResource", blockHandler = "doActionBlockHandler", fallback = "doActionFallback")
+    public String doAction(@PathVariable("p1") Integer p1) {
+        if (p1 == 0) {
+            throw new RuntimeException("p1 is 0");
+        }
+        return "doAction";
+    }
+
+    public String doActionFallback(@PathVariable("p1") Integer p1, BlockException e) {
+        log.error("sentinel配置自定义限流了：" + e);
+        return "程序逻辑异常了" + "\t" + e.getMessage();
+    }
+
+    public String doActionFallback(@PathVariable("p1") Integer p1, Throwable e) {
+        log.error("逻辑程序异常了：" + e);
+        return "程序逻辑异常了" + "\t" + e.getMessage();
+    }
+
+
+    @GetMapping(value = "/test/HotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealHandler_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2
+    ) {
+        return "------testHotKey------";
+    }
+
+    public String dealHandler_testHotKey(String p1, String p2, BlockException e) {
+        return "----dealHandler_testHotKey";
+    }
+
+
 
 }
