40a0e0c56849f0d10dba5a93b00ded3ab9d0e919
Jiligu <2497050447@qq.com>
2024-10-21 00:06:18 +0800

    多线程
    2024年10月21日00:06:16

--- Multithreading/01Thread/src/main/java/com/waitAndNotify/Cook.java
+++ Multithreading/01Thread/src/main/java/com/waitAndNotify/Cook.java
@@ -4,6 +4,25 @@ public class Cook extends Thread {
 
     @Override
     public void run() {
-        super.run();
+        while (true) {
+            synchronized (Desk.lock){
+                if (Desk.count == 0){
+                    break;
+                }else{
+                    if (Desk.foodFlag == 1){
+                        try {
+                            Desk.lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }else{
+                        //  没有就做
+                        System.out.println("Cook做了一碗面条");
+                        Desk.foodFlag = 1;
+                        Desk.lock.notifyAll();
+                    }
+                }
+            }
+        }
     }
 }
--- Multithreading/01Thread/src/main/java/com/waitAndNotify/ThreadDemoWaitAndNotify.java
+++ Multithreading/01Thread/src/main/java/com/waitAndNotify/ThreadDemoWaitAndNotify.java
@@ -6,5 +6,11 @@ public class ThreadDemoWaitAndNotify {
             需求：完成生产者和消费者（等待唤醒机制）代码
                 实现线程轮流交替执行的效果
          */
+        Cook c = new Cook();
+        Foodie f = new Foodie();
+        c.setName("厨师");
+        f.setName("吃货");
+        c.start();
+        f.start();
     }
 }
