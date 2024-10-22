ab2d816f66666578289902d2153f112f490c912a
Jiligu <2497050447@qq.com>
2024-10-22 22:44:16 +0800

    多线程
    2024年10月22日22:44:10

---
+++ Multithreading/01Thread/src/main/java/com/test04/testMain04.java
@@ -0,0 +1,18 @@
+package com.test04;
+
+public class testMain04 {
+    public static void main(String[] args) {
+        /*
+            微信中的抢红包也用到了线程。
+            假设：100，分成3个包，现在有5个人去抢
+            其中，红包是共享数据。
+            5个人就5条县城
+            打印结果如下：
+                xxx抢到xx元
+                xxx抢到xx元
+                xxx抢到xx元
+                xxx没抢到
+                xxx没抢到
+         */
+    }
+}
