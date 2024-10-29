a80f14b11a11d1783cbe7a824b176212d5e86bb1
Jiligu <2497050447@qq.com>
2024-10-29 23:00:24 +0800

    TCP
    2024年10月29日23:00:22

--- socketnet/src/main/java/com/tcp/test03/testServer03.java
+++ socketnet/src/main/java/com/tcp/test03/testServer03.java
@@ -1,8 +1,33 @@
 package com.tcp.test03;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class testServer03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //  客户端：将本地文件上传到服务器。接收服务器的反馈
         //  服务器：接收客户端上传的文件，上床完毕之后给出反馈
+
+        ServerSocket ss = new ServerSocket(10086);
+
+        Socket socket = ss.accept();
+        BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+        String path = "K:\\GitHub\\notes\\socketnet\\server\\1.png";
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path));
+        int len;
+        byte[] buffer = new byte[1024];
+        while ((len = bis.read(buffer)) != -1) {
+            bos.write(buffer, 0, len);
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        bw.write("上传成功");
+        bw.newLine();
+        bw.flush();
+
+        socket.close();
+        ss.close();
+
     }
 }
