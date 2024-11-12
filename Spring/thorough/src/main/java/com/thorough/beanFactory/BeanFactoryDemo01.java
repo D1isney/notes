a6b1c1a0ef4f5bbea6155ff7244b9cd868ff166c
Jiligu <2497050447@qq.com>
2024-11-12 22:43:52 +0800

    Spring5底层
    2024年11月12日22:43:37
    容器实现

--- Spring/thorough/src/main/java/com/thorough/beanFactory/BeanFactoryDemo01.java
+++ Spring/thorough/src/main/java/com/thorough/beanFactory/BeanFactoryDemo01.java
@@ -1,33 +1,3 @@
-# Spring底层
-
-## 1、容器接口
-
-### 1.1、BeanFactory能做什么
-
-> 什么是BeanFactory
->
-> - 它是ApplicationContext的父接口
-> - 它才是Spring的核心容器，主要的ApplicationContext实现都【结合】了它的功能
-
-> BeanFactory能做什么
->
-> - 表面上只有getBean
-> - 实际上控制反转、基本的依赖注入、直至Bean的生命周期的各种功能，都是由它的实现类提供
-
-### 1.2、ApplicationContext有哪些扩展功能
-
-- 国际化
-- 通配符获取资源
-- 获取配置信息
-- 事件发布
-
-
-
-## 2、容器实现
-
-### 2.1、BeanFactory实现的特点
-
-```java
 package com.thorough.beanFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -124,17 +94,5 @@ public class BeanFactoryDemo01 {
             System.out.println("Bean2 Constructor");
         }
     }
+
 }
-```
-
-- BeanFactory不会主动调用BeanFactory后处理器
-- BeanFactory不会主动添加Bean后处理器
-- BeanFactory不会主动初始化单例
-- BeanFactory不会解析beanFactory 还不会解析 ${ } 与 #{ }
-- bean后处理器会有排序的逻辑
-
-
-
-### 2.2、ApplicationContext的常见实现和用法
-
-### 2.3、内嵌容器、注册DispatcherServlet
