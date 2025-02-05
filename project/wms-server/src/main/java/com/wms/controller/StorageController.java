b799783743cc366eb789873613d28a8860a981f9
Jiligu <2497050447@qq.com>
2025-02-05 22:12:00 +0800

    1
    1

--- project/wms-server/src/main/java/com/wms/controller/StorageController.java
+++ project/wms-server/src/main/java/com/wms/controller/StorageController.java
@@ -1,11 +1,39 @@
 package com.wms.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.wms.service.StorageService;
+import com.wms.utils.PageUtil;
+import com.wms.utils.Query;
+import com.wms.utils.R;
+import com.wms.vo.StorageVo;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/storage")
 @Api(tags = "Controller-库位信息", value = "/storage")
 public class StorageController {
+    @Resource
+    private StorageService storageService;
+
+    @ApiOperation("查询数据")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "code", value = "库位编码")
+    })
+    @GetMapping("list")
+    public R<?> list(@RequestParam Map<String, Object> params) {
+        Query query = new Query(params);
+        IPage<StorageVo> page = storageService.pageList(query.getIPage(StorageVo.class), query);
+        PageUtil pageUtil = new PageUtil(page.getRecords(), page.getTotal(), query.getLimit(), query.getPage());
+        return R.ok(pageUtil);
+    }
 }
